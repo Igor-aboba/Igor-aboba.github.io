b97dd51e77bb2447ae2434afefa780cb31427232
Igor-aboba <108731475+Igor-aboba@users.noreply.github.com>
2022-08-16 12:52:55 +0600

    Переперезалил 6 ДЗ

--- Source/js/MyScript.js
+++ Source/js/MyScript.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 
     /* Загрузчик */
+    
 $(".preloader").css("display", "none");   
     
         
   /* Калькулятор */ 
+    
 function calculate(){
     
 let sum = parseInt($("#select1 option:selected").val()) + parseInt($("#select2 option:selected").val()) + parseInt($("#select3 option:selected").val());
@@ -22,6 +24,7 @@ calculate();
     
 
   /* Подсвечивание */
+    
 $(window).scroll(() => {
    
     let scrollDistance = $(window).scrollTop();
@@ -48,21 +51,60 @@ $(window).scroll(() => {
   /* Отложенная анимация */
     
     let options = {threshold: [1]};
-    let observer = new IntersectionObserver(onEnttry, options);
-    let elements = $('.skills');
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.element-animation');
     elements.each((i, el) => {
        observer.observe(el); 
     });
     
-   
-    
- }); 
-
-function onEntry (entry){
+    function onEntry (entry){
     entry.forEach(change => {
        if (change.isIntersecting){
-           change.target.classList.add('show-animation')
+           change.target.classList.add('show-animation');
        } 
     });
 }
+   
+    /* Модальное окно по таймеру */
+    
+    setTimeout(function () {
+        const myModal = new bootstrap.Modal('#Modal', {
+            keyboard: false
+        });
+       modalToggle = document.getElementById('togglemodal');
+        myModal.show(modalToggle);
+        
+    }, 3000);
+    
+    /* Бегущие цифры */
+    
+     let optionsRun = {
+            threshold: [0.5]
+        };
+        let observerRun = new IntersectionObserver(onEntryStat, optionsStat);
+        let elementsRun = $('.RunAnimation');
+
+        elementsRun.each((i, el) => {
+            observerRun.observe(el);
+        });
+
+
+        function onEntryStat(entry) {
+            entry.forEach(change => {
+                if (change.isIntersecting) {
+                    if(!$('.RunAnimation').hasClass("done")){
+                        $('.RunAnimation').addClass("done");
+                        $('.RunAnimation').spincrement({
+                         thousandSeparator: "",
+                         duration: 3000
+                    });
+                  }
+                }
+            });
+        }
+    
+   
+ }); 
+
+
 
